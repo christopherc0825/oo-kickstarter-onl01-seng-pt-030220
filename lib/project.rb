7e597580ec2822b065a9c1ff37d79f9e00970055
Christopher Chaison <cchaison0@gmail.com>
2020-03-16 23:00:20 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,6 +9,6 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    Backer.add_project(self) if Backer.backed_projects.include?(!self)
+    Backer.add_project(self) if !Backer.backed_projects.include?(self)
   end
 end
